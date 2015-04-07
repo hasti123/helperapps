67073f5b09dee78f1e5231ec3610c476d4929813
unknown <hastich@DW76168385.GuarantyOnline.com>
2015-04-07 16:22:32 -0500

    Add test csv to test method, rename method to make more readable, remove unused variables, add method arguments, and take out command line/user interaction code

--- countcommasinline.py
+++ countcommasinline.py
@@ -1,23 +1,17 @@
 #comma reader
-def findNumberCommas():
-    print("enter file name:")
-    filename = input()
-    #print("enter number of commas to match:")
-    #numCommas = input()
-
+#enter number of commas expected per line
+def findNumberCommasPerLine(numberOfCommasPerLine, filename):
     linenumber = 0
-    numCommas = 0
     errorLines = []
 
     with open(filename, 'r') as f:
         for line in f:
             linenumber = linenumber + 1
-            tempNumCommas = line.count(',')
-            if not tempNumCommas == 16:
+            numCommas = line.count(',')
+            if not numCommas == numberOfCommasPerLine:
                 errorLines.append(linenumber)
 
 
     print("Error lines: ")
     print(errorLines)
-    print("Number of errors:")
-    print(len(errorLines))
+    print("Number of errors: {0}".format(len(errorLines)))
