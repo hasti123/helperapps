0319f0e3cb21faecd763043dc6e7f0f44667e3d3
unknown <hastich@DW76168385.GuarantyOnline.com>
2015-04-07 11:42:33 -0500

    change to method so you can run more than once

--- countcommasinline.py
+++ countcommasinline.py
@@ -1,22 +1,23 @@
 #comma reader
-print("enter file name:")
-filename = input()
-print("enter number of commas to match:")
-numCommas = input()
+def findNumberCommas():
+    print("enter file name:")
+    filename = input()
+    #print("enter number of commas to match:")
+    #numCommas = input()
 
-linenumber = 0
-numCommas = 0
-errorLines = []
+    linenumber = 0
+    numCommas = 0
+    errorLines = []
 
-with open(filename, 'r') as f:
-    for line in f:
-        linenumber = linenumber + 1
-        tempNumCommas = line.count(',')
-        if not tempNumCommas == numCommas:
-            errorLines.append(linenumber)
+    with open(filename, 'r') as f:
+        for line in f:
+            linenumber = linenumber + 1
+            tempNumCommas = line.count(',')
+            if not tempNumCommas == 16:
+                errorLines.append(linenumber)
 
 
-print("Error lines: ")
-print(errorLines)
-print("Number of errors:")
-print(len(errorLines))
+    print("Error lines: ")
+    print(errorLines)
+    print("Number of errors:")
+    print(len(errorLines))
