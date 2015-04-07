93093263f48a45bd34ba8526b7af1073ff19f0d2
unknown <hastich@DW76168385.GuarantyOnline.com>
2015-04-07 11:23:03 -0500

    add app that counts commas in each line

---
+++ countcommasinline.py
@@ -0,0 +1,22 @@
+#comma reader
+print("enter file name:")
+filename = input()
+print("enter number of commas to match:")
+numCommas = input()
+
+linenumber = 0
+numCommas = 0
+errorLines = []
+
+with open(filename, 'r') as f:
+    for line in f:
+        linenumber = linenumber + 1
+        tempNumCommas = line.count(',')
+        if not tempNumCommas == numCommas:
+            errorLines.append(linenumber)
+
+
+print("Error lines: ")
+print(errorLines)
+print("Number of errors:")
+print(len(errorLines))
